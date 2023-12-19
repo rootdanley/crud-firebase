9575b1939e7f7059eff2e78089b97cbb3efede73
Danley Alves <danleyalves@hotmail.com>
2023-12-18 23:42:51 -0300

    Update environment.ts

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -3,11 +3,11 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyAXwRHpTQ6iiN8uLGtBO8nkOpUNdYXFLPY",
-    authDomain: "bitbitz.firebaseapp.com",
-    projectId: "bitbitz",
-    storageBucket: "bitbitz.appspot.com",
-    messagingSenderId: "689079155279",
-    appId: "1:689079155279:web:8fc1311c31d6c30568c83a"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   }
 };
